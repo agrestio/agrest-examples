cef236e70009506340460e926caa65a5d92340ce
Andrus Adamchik <andrus@objectstyle.com>
2022-02-06 13:39:53 +0200

    Initial commit of the multi-example project

---
+++ README.md
@@ -0,0 +1,5 @@
+# agrest-examples
+
+A collection of examples using [Agrest.io](https://agrest.io). Each example is a standalone runnable application that can built and started with Maven.
+
+* [agrest-jaxrs-cayenne](agrest-jaxrs-cayenne) - Agrest with JAX-RS API, Apache Cayenne backend, Apache Derby DB
---
+++ agrest-jaxrs-cayenne/src/main/java/org/example/agrest/Application.java
@@ -0,0 +1,26 @@
+package org.example.agrest;
+
+import io.agrest.cayenne.AgCayenneBuilder;
+import io.agrest.cayenne.AgCayenneModule;
+import io.agrest.runtime.AgBuilder;
+import io.agrest.runtime.AgRuntime;
+import org.apache.cayenne.configuration.server.ServerRuntime;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api/*")
+public class Application extends ResourceConfig {
+
+    public Application() {
+
+        ServerRuntime cayenneRuntime = ServerRuntime.builder()
+                .addConfig("cayenne-project.xml")
+                .build();
+        AgCayenneModule cayenneExt = AgCayenneBuilder.build(cayenneRuntime);
+        AgRuntime agRuntime = new AgBuilder().module(cayenneExt).build();
+
+        register(agRuntime);
+        packages("org.example.agrest.api");
+    }
+}
